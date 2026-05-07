72d4674a32c2aba472be06e9c8e2008ac6978d0d
AlanBenjaDev <alenciopalencio@gmail.com>
2026-05-07 17:54:20 -0300

    fix cors errors

--- Backend/src/Server/App.ts
+++ Backend/src/Server/App.ts
@@ -1,9 +1,17 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors'
-const app : Application = express()
+import express, { Application } from 'express';
+import cors from 'cors';
 import router from '../Routes/appointment.routes.js';
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/appointments', router)
-export default app
+const app: Application = express();
+
+// Configuración de CORS
+app.use(cors({
+  origin: 'https://taiwan-barber.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json());
+app.use('/api/appointments', router);
+
+export default app;
